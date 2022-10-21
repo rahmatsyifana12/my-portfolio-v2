f1feba5d0f7fec5b665293b1791bb5cb1791b47e
rahmatsyifana12 <adean435@gmail.com>
2022-10-21 11:09:38 +0700

    Add about component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
+import About from "../About/About";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 import HomeSection from "./HomeSection";
 
 function Home() {
     return (
-        <div className="">
+        <div>
             <Navbar />
             <HomeSection />
+            <About />
             <Footer />
         </div>
     );
