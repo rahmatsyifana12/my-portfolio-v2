99336405bcf059b40631748e9b1fab1ca593599d
rahmatsyifana12 <adean435@gmail.com>
2022-10-21 18:38:49 +0700

    Add scroll to top component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Portfolio from './components/Portfolio/Portfolio';
 import About from './components/About/About';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 function App() {
   return (
     <div>
+      <ScrollToTop>
         <Routes>
           <Route exact path='/'>
             <Route exact path='/' element={<Home />} />
@@ -13,6 +15,7 @@ function App() {
             <Route exact path='/portfolio' element={<Portfolio />} />
           </Route>
         </Routes>
+      </ScrollToTop>
     </div>
   );
 }
